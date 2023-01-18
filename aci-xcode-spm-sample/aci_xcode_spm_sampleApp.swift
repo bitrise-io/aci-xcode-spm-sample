5f5c526b1a256ac1d71a680695fdafaf46bf70d4
Zach Gray <zach.gray@bitrise.io>
2023-01-18 11:36:12 +1100

    Initial Commit

---
+++ aci-xcode-spm-sample/aci_xcode_spm_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  aci_xcode_spm_sampleApp.swift
+//  aci-xcode-spm-sample
+//
+//  Created by Zach Gray on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct aci_xcode_spm_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
